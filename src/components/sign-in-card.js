01ebb858056360d4392293c849c3332bb48c4bf4
Josh George <joshgeorge987@gmail.com>
2023-07-07 14:11:16 +0530

    sign in component added

--- src/components/sign-in-card.js
+++ src/components/sign-in-card.js
@@ -10,13 +10,15 @@ export default function SignInCard() {
                     <h3 className="sign-h3 in">View your profile details</h3>
                 </div>
                 <div className="email">
+                    <label htmlFor="email"></label>
                     <input type="text" id="email" autoComplete="off" placeholder="Email"></input>
                 </div>
                 <div className="pw">
+                    <label htmlFor="pw"></label>
                     <input type="password" id="pw" autoComplete="off" placeholder="Password"></input>
                 </div>
                 <div className="btn-submit">
-                    <button type="submit" id="signin-submit" className="btn btn-default">Sign in</button>
+                    <button type="submit" id="sign-submit" className="btn btn-default">Sign in</button>
                 </div>
             </div>
         </form>
